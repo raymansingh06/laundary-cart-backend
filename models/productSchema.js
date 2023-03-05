6da5578023993dc161c32cebdbd7553bd0e64faf
raymansingh06 <102753103+raymansingh06@users.noreply.github.com>
2023-03-04 22:21:40 -0800

    Add files via upload

---
+++ models/productSchema.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema();
+
+
+const ProductModel = mongoose.model("product_types",productSchema);
+
+module.exports = ProductModel; 
---
+++ server.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+const port = process.env.PORT || 3001
+const API = process.env.DATABASE_URL || "mongodb+srv://Chandini_S:Crystal0212@cluster0.l2pby5u.mongodb.net/laundrycart?retryWrites=true&w=majority" // "mongodb+srv://Akanksha:Apawar123@cluster0.6karf8f.mongodb.net/test?retryWrites=true&w=majority"  // mine="mongodb+srv://Chandini_S:Crystal0212@cluster0.l2pby5u.mongodb.net/laundrycart?retryWrites=true&w=majority"
+
+mongoose.set('strictQuery', false);
+ 
+const app = require('./app');
+dotenv.config();
+
+async function main() {
+    await mongoose.connect(API);
+    console.log('connected to database');
+    app.listen(port, () => console.log(`Server is live at PORT => ${port}`));
+};
+main();
